8ce259f1da6256596e261a64acc21d65140fa346
santocaridi99 <santocaridi99@gmail.com>
2022-03-04 17:10:53 +0100

    aggiunto un pò di stile Scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,16 @@
 @extends('layouts.default')
 {{-- sezione principale --}}
 @section('content')
-<main>
-    <div class="card-container">
-        @foreach ($data as $movie)
-        <div class="card">
-            <h1>{{$movie->id}}</h1>
-            <h1>{{$movie->title}}</h1>
-            <p>{{$movie->original_title}}</p>
-            <p>{{$movie->date}}</p>
-            <p>{{$movie->vote}}</p>
-        </div>
-        @endforeach
+<div class="card-container">
+    @foreach ($data as $movie)
+    <div class="card">
+        <h6>{{$movie->id}}</h6>
+        <h4>{{$movie->title}}</h4>
+        <p>Titolo originale:{{$movie->original_title}}</p>
+        <p>Data{{$movie->date}}</p>
+        <p>Voti:{{$movie->vote}}</p>
     </div>
-</main>
+    @endforeach
+</div>
+
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Movie</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
+
 <body>
     <header>
-        <h1>qui andrà un header</h1>
+        {{-- qui andrà un header --}}
     </header>
     <main>
         @yield('content')
     </main>
     <footer>
-        <h1>qui andrà un footer</h1>
+       {{-- qui andrà un footer --}}
     </footer>
 </body>
+
 </html>
